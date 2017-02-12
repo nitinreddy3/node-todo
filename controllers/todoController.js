fd7cc06eaa2db17d16e2028ed61c0c1456f03792
nitin reddy <reddynitin3@gmail.com>
2017-02-12 13:35:11 +0530

    Added views for todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,10 +1,16 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'nitin'}, {item:'common'}, {item: 'foff'}]
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app) {
     app.get('/todo', function(req, res) {
-
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo', function(req, res) {
-        
+    app.post('/todo', urlencodedParser, function(req, res) {
+        data.push(req.body);
+        res.json(data);
     });
 
     app.delete('/todo', function(req, res) {
